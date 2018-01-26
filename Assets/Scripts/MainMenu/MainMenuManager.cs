8c3da923b7fcadeb5db7d0f6a1ae71ae7542c3a2
Pierre Galaup <contact@pierre-galaup.fr>
2018-01-26 21:05:38 +0100

    Projet de base avec 3D + mainmenu basique

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace MainMenu
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+        public void StartGame()
+        {
+            SceneManager.LoadScene("Game");
+        }
+
+        public void QuitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
