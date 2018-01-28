dce72b2b6a907cbd01a9c8117fb06e1f7568544e
Pierre Galaup <contact@pierre-galaup.fr>
2018-01-28 01:20:17 +0100

    Affichage des infos sur les cellules

--- Assets/Scripts/Map/MapManager.cs
+++ Assets/Scripts/Map/MapManager.cs
@@ -1,7 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using BusinessCore;
 using Game;
+using Town;
+using Translation;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Map
 {
@@ -27,6 +31,24 @@ namespace Map
         [SerializeField]
         private GameObject _infosMenu;
 
+        [SerializeField]
+        private GameObject _disctrictMenu;
+
+        [SerializeField]
+        private Text _infoNameText;
+
+        [SerializeField]
+        private Text _infoTypeText;
+
+        [SerializeField]
+        private Text _infoMaintenanceCostText;
+
+        [SerializeField]
+        private Text _infoLevelDistrictText;
+
+        [SerializeField]
+        private Text _infoPeopleDistrictText;
+
         private Dictionary<Vector2, GameObject> _mapCells;
         private GameObject _map;
         private Cell _currentCell = null;
@@ -40,6 +62,7 @@ namespace Map
         {
             _infosMenu.SetActive(false);
             _buildMenu.SetActive(false);
+            _disctrictMenu.SetActive(false);
         }
 
         public Cell GetCell(float x, float y)
@@ -88,6 +111,7 @@ namespace Map
         public void ClickOnCell(Cell cell)
         {
             _buildMenu.SetActive(false);
+            _disctrictMenu.SetActive(false);
             _infosMenu.SetActive(false);
 
             if (_currentCell == cell)
@@ -107,7 +131,11 @@ namespace Map
 
             if (!cell.IsConstructible)
             {
-                // TODO : Display info sur le quartier
+                District district = cell.Building.GetComponent<District>();
+                _infoLevelDistrictText.text = district.Level.ToString(CultureInfo.InvariantCulture);
+                _infoPeopleDistrictText.text = district.Peoples.ToString(CultureInfo.InvariantCulture);
+
+                _disctrictMenu.SetActive(true);
             }
             else if (!cell.HaveBuilding) // Si la cellule n'a rien dessus et qu'elle est constructible
             {
@@ -115,6 +143,12 @@ namespace Map
             }
             else if (cell.HaveBuilding) // Si la cellule a un truc dessus
             {
+                IInfrastructure infrastructure = cell.Building.GetComponent<IInfrastructure>();
+
+                _infoMaintenanceCostText.text = infrastructure.MaintenanceCost.ToString(CultureInfo.InvariantCulture);
+                _infoNameText.text = infrastructure.Name;
+                _infoTypeText.GetComponent<TranslateText>().SetText(infrastructure.InfrastructureType == InfrastructureType.CellularNetwork ? "Cellular" : "Wired");
+
                 _infosMenu.SetActive(true);
             }
         }
